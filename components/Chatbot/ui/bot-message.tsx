c2985a446069108c868ac12bd770c183eb8e1629
Milana <milka3341@gmail.com>
2025-01-18 07:44:05 +0200

    last

--- components/Chatbot/ui/bot-message.tsx
+++ components/Chatbot/ui/bot-message.tsx
@@ -1,9 +1,3 @@
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-import remarkMath from 'remark-math'
-import rehypeKatex from 'rehype-katex'
-import 'katex/dist/katex.min.css'
-
 export default function BotMessage({role, content}: {role: string, content: string}){
     return(
             <div className="flex w-full p-3 my-2">
